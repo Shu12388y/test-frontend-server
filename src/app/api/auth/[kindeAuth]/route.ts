6c61d9d1902ad7817a6f0c0aa7736c7daaf5416c
Shu12388y <spl.sp999@gmail.com>
2024-06-28 22:14:46 +0530

    fix

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,6 +1,8 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
-export async function GET(request:NextRequest,{params}:any){
-  const endpoint = params.kindeAuth
-  return handleAuth(request,endpoint)
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params }: any): Promise<Response> {
+  const endpoint = params.kindeAuth;
+  const response = await handleAuth(request, endpoint);
+  return NextResponse.json(response);
 }
